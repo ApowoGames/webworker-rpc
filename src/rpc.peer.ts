f89ea31c0a50dd9199747028eb63d8a89831ceee
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2021-03-06 14:35:06 +0800

    workerurl直接使用传入值

--- src/rpc.peer.ts
+++ src/rpc.peer.ts
@@ -847,17 +847,17 @@ export class RPCPeer extends RPCEmitter {
             return;
         }
 
-        console.log("webworker-rpc ProxyCreateWorker: location: ", location);
-        let pathRoot = location.origin + location.pathname;
-        // console.log("webworker-rpc ProxyCreateWorker: pathRoot-1: ", pathRoot);
-        // 兼容ios打包
-        if (pathRoot.endsWith("index.html")) {
-            pathRoot = pathRoot.slice(0, -10);
-        } else if (pathRoot.endsWith("index.html/")) {
-            pathRoot = pathRoot.slice(0, -11) + "/";
-        }
+        // console.log("webworker-rpc ProxyCreateWorker: location: ", location);
+        // let pathRoot = location.origin + location.pathname;
+        // // console.log("webworker-rpc ProxyCreateWorker: pathRoot-1: ", pathRoot);
+        // // 兼容ios打包
+        // if (pathRoot.endsWith("index.html")) {
+        //     pathRoot = pathRoot.slice(0, -10);
+        // } else if (pathRoot.endsWith("index.html/")) {
+        //     pathRoot = pathRoot.slice(0, -11) + "/";
+        // }
         // console.log("webworker-rpc ProxyCreateWorker: pathRoot-2: ", pathRoot);
-        const path = pathRoot + workerUrl;
+        const path = workerUrl;
         // console.log("webworker-rpc ProxyCreateWorker: workerUrl: ", workerUrl);
         const newWorker = new Worker(path, {name: workerName});
         console.log(this.name + " create worker: ", path, workerName);
